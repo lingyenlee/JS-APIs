8f3aae42bf843811ad95821219e37b6be37177b5
lingyenlee <lingyenlee@gmail.com>
2020-06-09 23:06:25 +1000

    include search

--- countryFinder/index.js
+++ countryFinder/index.js
@@ -1,19 +1,41 @@
-const countryURL = "https://restcountries.eu/rest/v2/name/";
+const countryForm = document.querySelector("form")
 const details = document.querySelector(".details")
 const cardTitle = document.querySelector(".card-title")
+const card = document.querySelector(".card")
+const cardBody = document.querySelector(".card-body")
+const flagImage = document.createElement("img")
+
+const countryURL = "https://restcountries.eu/rest/v2/name/";
 
 //handle error response
 const handleErrors = function (response) {
-    if(!response.ok) {
-        throw (response.status + ': ' + response.statusText )
+    if (!response.ok) {
+        throw (response.status + ': ' + response.statusText)
     }
     return response.json()
 }
 
 //handle success response
 const handleSuccess = function (data) {
+    //get country flag
+    const countryFlag = data[0].flag
+    //console.log(countryFlag)
+    flagImage.setAttribute("src", countryFlag)
+    flagImage.classList.add("card-img-top")
+    card.insertBefore(flagImage, cardBody)
+
+    //get country details
     const countryName = data[0].name
-    const pop = Math.round(data[0].population/1000000).toFixed(1)
+    const pop = () => {
+        if (data[0].population >= 1000000) {
+            return Math.round(data[0].population / 1000000).toFixed(1) + " million"
+        } else {
+            return data[0].population + " thousand"
+        }
+    }
+
+
+
     const cap = data[0].capital
     const continent = data[0].region
     const currency = data[0].currencies[0].name
@@ -25,13 +47,18 @@ const handleSuccess = function (data) {
     cardTitle.innerHTML = `${countryName}`
     details.innerHTML = `
     <ul>
-    <li>Capital: ${cap} </li>
-    <li>Region: ${continent}</li>
-    <li>Population: ${pop} million</li>
-    <li>Languages: ${language}  </li>
-    <li>Currency: ${currency} (${currencySymbol})</li>
-    <li>Calling codes: ${code}</li>
-</ul>`
+        <li>Capital: ${cap} </li>
+        <li>Region: ${continent}</li>
+        <li>Population: ${pop()} </li>
+        <li>Languages: ${language}  </li>
+        <li>Currency: ${currency} (${currencySymbol})</li>
+        <li>Calling codes: ${code}</li>
+    </ul>`
+
+
+    if (card.classList.contains("d-none")) {
+        card.classList.remove("d-none")
+    }
 }
 
 const getCountry = function (countryName) {
@@ -42,46 +69,11 @@ const getCountry = function (countryName) {
         .then(data => handleSuccess(data))
 }
 
+countryForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let country = countryForm.country.value.trim()
+    countryForm.reset()
 
-getCountry("Belgium")
+    getCountry(country)
 
-
-
-
-
-// function getCountry(url) {
-//     return new Promise((resolve, reject) => {
-//         let request = new XMLHttpRequest();
-//         request.open('GET', url)
-//         request.onload = () => {
-//             if (request.status === 200) {
-//                 resolve(request.response)
-//             } else {
-//                 reject(Error(request.statusText))
-//             }
-//         }
-
-//         //handle network errors
-//         request.onerror = function() {
-//             reject(Error('Network error'))
-//         }
-
-//         request.send()
-//     })
-// }
-
-// function successHandler(data) {
-//     const dataObj = JSON.parse(data)
-//     console.log(dataObj)
-// }
-
-// function failHandler(data) {
-//     console.log(data)
-// }
-
-// getCountry("https://restcountries.eu/rest/v2/name/eesti")
-//     .then(response => successHandler(response))
-//     .catch(error => failHandler(error))
-
-
-
+})
